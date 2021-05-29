fefe4cdd30f98b8794b0af6fcd7ef752fa0023aa
Daniel Kim <>
2021-05-29 20:12:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+// My Status: 
+// 1 - FULL CREDIT
+// 2 - PARTIAL CREDIT
+// 3 - 0 CREDIT, BUT DID SOME CODING
+// 4 - READ, UNDERSTOOD AND THOUGHT ABOUT IT
+// 5 - TOO LONG, DIDNT READ
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
